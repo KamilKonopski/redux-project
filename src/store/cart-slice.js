b4e2afaa948fce9530535ffb0f890535f0e87fa9
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-28 01:33:10 +0200

    repare little problem

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -9,22 +9,26 @@ const cartSlice = createSlice({
 		addItemtoCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find((item) => item.id === newItem.id);
+			state.totalQuantity++;
+
 			if (!existingItem) {
 				state.items.push({
-					itemId: newItem.id,
+					id: newItem.id,
 					price: newItem.price,
 					quantity: 1,
 					totalPrice: newItem.price,
 					name: newItem.name,
 				});
 			} else {
-				existingItem.quantity = existingItem.quantity++;
+				existingItem.quantity++;
 				existingItem.totalPrice = existingItem.totalPrice + newItem.price;
 			}
 		},
 		removeItemFromCart(state, action) {
 			const id = action.payload;
 			const existingItem = state.items.find((item) => item.id === id);
+			state.totalQuantity--;
+
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter((item) => item.id !== id);
 			} else {
