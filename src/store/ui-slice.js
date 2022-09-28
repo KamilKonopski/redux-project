a681b9d083e83fb3f7ce4b00a900914616d60ed0
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-28 14:14:52 +0200

    add notification feature

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartInitialState = { cartIsVisible: false };
+const cartInitialState = { cartIsVisible: false, notification: null };
 
 const uiSlice = createSlice({
 	name: "ui",
@@ -8,6 +8,13 @@ const uiSlice = createSlice({
 	reducers: {
 		toggleVisible(state) {
 			state.cartIsVisible = !state.cartIsVisible;
+		},
+		showNotification(state, action) {
+			state.notification = {
+				status: action.payload.status,
+				title: action.payload.title,
+				message: action.payload.message,
+			};
 		},
 	},
 });
